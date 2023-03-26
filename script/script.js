15275db34d6de53f3d5b3d39beeede65f9887fdd
AnnaMasko <maskohanna@gmail.com>
2023-03-26 18:54:15 +0300

    add todoList

---
+++ script/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const input = document.querySelector("input[type='text']");
+const div_todo = document.querySelector('div.todo');
+const clearButton = document.querySelector("button.clear");
+const ul = document.createElement('ul');
+input.after(ul);
+ul.classList.add('todo__items');
+
+
+
+input.addEventListener("keypress", (keyPressed) => {
+    const keyEnter = 13;
+    if (keyPressed.which == keyEnter) {
+        if(input.value !=="")
+        {createToDo();}
+        else{ alert('Повтори ввод');}   
+    }
+});
+
+function createToDo() {   
+    const chekbox = document.createElement('input');
+    chekbox.setAttribute('type', 'checkbox');
+    chekbox.classList.add('checkbox')
+    const li = document.createElement('li');
+    let textSpan = document.createElement("span");
+    textSpan.classList.add('item');
+    textSpan.append(input.value);
+    ul.appendChild(li).append(chekbox, textSpan);
+    if (input.value === input.value) {
+        input.value = '';
+    }
+
+    chekbox.addEventListener("click", () => {
+        li.style.textDecoration = "line-through";
+        if (chekbox.checked === false) {
+            li.style.textDecoration = "none";
+        }
+    }
+    )
+
+    textSpan.addEventListener("click", () => {
+        textSpan.innerHTML = "";
+        textSpan.append(prompt('Измени задачу'));
+        ul.appendChild(li).append(chekbox, textSpan);
+    }
+    )
+}
+
+
+clearButton.addEventListener("click", () => {
+    ul.innerHTML = "";
+    localStorage.removeItem('todos', ul.innerHTML);
+});
+
+
+
